41d53d122b0643e7aafbf424138816f6a866f2d3
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2015-04-24 14:46:08 +0200

    svg.hs cleanup

--- artdoc/data/bin/svg.hs
+++ artdoc/data/bin/svg.hs
@@ -6,15 +6,18 @@ import Text.Pandoc
 import Text.Pandoc.JSON
 
 toSVGImage :: String -> IO String
-toSVGImage filename = do svgfilename <- return (replaceExtension filename ".svg")
-                         svgexists <- doesFileExist svgfilename
-                         return (if svgexists then svgfilename else filename)
+toSVGImage filename = do 
+    let svgfilename = replaceExtension filename ".svg"
+    svgexists <- doesFileExist svgfilename
+    if svgexists 
+        then return svgfilename 
+        else return filename
 
 toSVG :: Inline -> IO Inline
-toSVG (Image inlines (target, title)) = 
-    do new_target <- toSVGImage(target)
-       return (Image inlines (new_target, title))
-toSVG x = do return x
+toSVG (Image inlines (target, title)) = do 
+    new_target <- toSVGImage(target)
+    return (Image inlines (new_target, title))
+toSVG x = return x
 
 main :: IO ()
 main = toJSONFilter toSVG
