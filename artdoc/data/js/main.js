dfe5f49392aeb17be9eef6bb57d69f2cba17ec6d
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2015-04-30 09:32:01 +0200

    Mathjax loader icon

---
+++ artdoc/data/js/main.js
@@ -0,0 +1,20 @@
+
+$(function() {
+  icon = $(
+             "<i></i>", 
+             {
+               "id": "mathjax-working",
+               "class": "fa fa-cog fa-spin",
+               "style": "font-size:1em;" + 
+                        "position:fixed;" +
+                        "top:1em;" +
+                        "left:1em;"
+             }
+          ).prependTo("body");
+
+   MathJax.Hub.Register.StartupHook(
+     "End", 
+     function () {
+       $("#mathjax-working").remove();
+     });
+});
