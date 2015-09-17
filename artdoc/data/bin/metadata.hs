2048a14fff7a817d96831a6b4b078ec7ffeb29ad
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2015-09-17 13:35:07 +0200

    better pandoc metadata reader

--- artdoc/data/bin/metadata.hs
+++ artdoc/data/bin/metadata.hs
@@ -1,17 +1,40 @@
 #!/usr/bin/env runhaskell
 
-import Data.Map
-import Data.Maybe
-import System.IO
-import System.Environment
+import Data.Map (empty, lookup, Map(..), toList)
+import System.Console.GetOpt
+import System.Environment (getArgs)
 import Text.JSON
 import Text.Pandoc
-import Text.Pandoc.JSON
+--------------------------------------------------------------------------------
+-- TODO: it would be nice to support custom writer options via the command-line,
+--       but this feature of pandoc is only present in the script, not the 
+--       library. I don't feel like cut-and-pasting this huge chunk of logic
+--       here.
+-- TODO: reader options ? such as smart "-" ?
+--------------------------------------------------------------------------------
+data Opt = Opt { optWriter :: String }
 
--- TODO: output type (writer name) for fields that contain some Pandoc content.
--- TODO: need to specify HTML writer options (namely: set email obfuscation to none)
+defaultOpts :: Opt
+defaultOpts = Opt { optWriter = "html" }
 
-type StringWriter = WriterOptions -> Pandoc -> String
+optSpec :: [OptDescr (Opt -> IO Opt)]
+optSpec =     
+  [ 
+    Option "tw" ["to","write"]
+      (ReqArg (\arg opt -> return opt { optWriter = arg }) "FORMAT")
+      ""
+  ]
+
+--------------------------------------------------------------------------------
+type StringWriter = Pandoc -> String
+
+-- Rk: those two wraps would probably be a job for typeclasses ...
+wrapBlocks :: [Block] -> Pandoc
+wrapBlocks blocks = Pandoc noMeta blocks
+  where noMeta = Meta {unMeta = empty}
+
+wrapInlines :: [Inline] -> Pandoc
+wrapInlines inlines = wrapBlocks [Plain(inlines)]
 
 toJSON :: MetaValue -> StringWriter -> JSValue
 toJSON (MetaMap map) writer = JSObject (toJSObject (Prelude.map convert (toList map)))
@@ -20,37 +43,40 @@ toJSON (MetaList values) writer = JSArray (Prelude.map (\v -> toJSON v writer) v
 toJSON (MetaBool bool) writer = JSBool bool
 toJSON (MetaString string) _ = JSString (toJSString string)
 toJSON (MetaInlines inlines) writer = JSString (toJSString string)
-  where string = writer def (wrapInlines inlines)
+  where string = writer (wrapInlines inlines)
 toJSON (MetaBlocks blocks) writer = JSString (toJSString string)
-  where string = writer def (wrapBlocks blocks)
+  where string = writer (wrapBlocks blocks)
 
 jsConvert :: Pandoc -> StringWriter -> JSValue
 jsConvert (Pandoc Meta{unMeta=meta} blocks) writer = toJSON (MetaMap meta) writer 
 
-getFromMeta :: Pandoc -> String -> MetaValue
-getFromMeta (Pandoc meta blocks) key = fromJust (Data.Map.lookup key yaml)
-  where yaml = unMeta meta
-
-wrapBlocks :: [Block] -> Pandoc
-wrapBlocks blocks = Pandoc noMeta blocks
-  where noMeta = Meta {unMeta = Data.Map.empty}
-
-wrapInlines :: [Inline] -> Pandoc
-wrapInlines inlines = wrapBlocks [Plain(inlines)]
-
-customWriteHtmlString :: StringWriter
-customWriteHtmlString options doc = let
-    options' = options {writerEmailObfuscation = NoObfuscation}
-  in
-    writeHtmlString options' doc
+writerOptions :: WriterOptions -- hard-coded :(
+writerOptions = def {writerEmailObfuscation = NoObfuscation}
 
 main :: IO ()
 main = do  
-    args <- getArgs
-    let filename = (args !! 0)
-    txt <- readFile filename
-    let maybeDoc = readMarkdown def txt
-    case maybeDoc of
-      Left pandocError -> error (show pandocError)
-      Right doc -> putStrLn (encode (jsConvert doc customWriteHtmlString))
+       rawArgs <- getArgs
+       let (actions, args, errors) =  getOpt Permute optSpec rawArgs
+
+       let filename = (args !! 0)
+       txt <- readFile filename
+
+       let maybeDoc = readMarkdown def txt
+       let doc = case maybeDoc of (Left pandocError) -> error (show pandocError)
+                                  (Right doc') -> doc'
+
+       -- Cargo cult 
+       opts <- foldl (>>=) (return defaultOpts) actions
+       let Opt { optWriter = writerName } = opts
+
+       let writerOrError = getWriter writerName
+       case writerOrError of
+          Left errorMessage -> error errorMessage
+          Right writerMix -> let 
+              PureStringWriter writerFactory = writerMix
+              writer = writerFactory writerOptions
+              jsValue = jsConvert doc writer
+              string = Text.JSON.encode jsValue
+            in
+              putStrLn string
 
